9c6b661bab8ee0ba8df269f7bd1dded9498e824f
idle947 <idle947@gmail.com>
2021-08-31 19:45:57 +0900

    modify

--- python/Level2/2개 이하로 다른 비트.py
+++ python/Level2/2개 이하로 다른 비트.py
@@ -1,15 +1,20 @@
 # https://programmers.co.kr/learn/courses/30/lessons/77885
 
+
 def solution(numbers):
     answer = []
     for number in numbers:
         end = bin(number)[-2:]
         if end == '11':
             a = bin(number).replace(
-                '0b', '0')[::-1].replace('10', '01', 1)[::-1]
+                '0b', '0')
+            a = a[::-1]
+            a = a.replace('10', '01', 1)
+            a = a[::-1]
             answer.append(int(a, 2))
         else:
             answer.append(number+1)
+
     return answer
     # return [int(bin(n).replace(
     #             '0b', '0')[::-1].replace('10', '01', 1)[::-1], 2) if bin(n)[-2:] == '11' else n+1 for n in numbers]
